643f436594f50345e3ddcef3a895ee14e4f73033
Julia <d10532238@gapps.fg.tp.edu.tw>
2023-05-14 17:34:24 +0200

    regression tree

---
+++ flight_fare_prediction.py
@@ -0,0 +1,73 @@
+# Flight fare prediction
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+# Preprocessing
+# import dataset
+df = pd.read_excel("Data_Train.xlsx")
+
+# drop null value
+df.dropna(inplace=True)
+
+# drop duplicates
+df.drop_duplicates(keep='first', inplace=True)
+
+# convert Dep_Time and Arrival_Time into hour and minute
+# Dep_Time
+df["Dep_hour"] = pd.to_datetime(df["Dep_Time"]).dt.hour
+df["Dep_minute"] = pd.to_datetime(df["Dep_Time"]).dt.minute
+df = df.drop(columns="Dep_Time")
+
+# Arrival_Time
+df["Arr_hour"] = pd.to_datetime(df["Arrival_Time"]).dt.hour
+df["Arr_minute"] = pd.to_datetime(df["Arrival_Time"]).dt.minute
+df = df.drop(columns="Arrival_Time")
+
+# convert Date_of_Journey into timestamp
+df["Date_of_Journey_Year"] = pd.to_datetime(
+    df["Date_of_Journey"], format="%d/%m/%Y").dt.year
+# ps. I don't think "year" is mandatory
+df["Date_of_Journey_Month"] = pd.to_datetime(
+    df["Date_of_Journey"], format="%d/%m/%Y").dt.month
+df["Date_of_Journey_Day"] = pd.to_datetime(
+    df["Date_of_Journey"], format="%d/%m/%Y").dt.day
+df = df.drop(columns="Date_of_Journey")
+
+
+# convert Duration into minute
+df['Duration'] = df['Duration']\
+    .str.replace("h", '*60')\
+    .str.replace(' ', '+')\
+    .str.replace('m', '*1')\
+    .apply(eval)
+
+# drop Additional_Info 
+df.drop(['Additional_Info'], axis=1, inplace=True)
+
+# Encode categorical attributes
+
+# Select categorical data
+df_categorical = df[['Airline', 'Source', 'Destination']]
+df.drop(['Airline', 'Source', 'Destination'], axis=1, inplace=True)
+
+
+# Encode
+df_categorical = pd.get_dummies(df_categorical, drop_first=True)
+df['Total_Stops'] = df['Total_Stops'].map({"non-stop":0, "1 stop": 1, "2 stops": 2, "3 stops":3, "4 stops": 4})
+
+# Concat categorical and numerical data
+preprocessed = pd.concat([df, df_categorical], axis=1)
+target = df['Price']
+
+# Drop 'Price' because price is the target variable
+preprocessed.drop(['Price'], axis=1, inplace=True)
+
+# Train_Test_Split
+data_train, data_test, target_train, target_test = train_test_split(
+    preprocessed, target,test_size=0.3, random_state=42)
+
+
+
